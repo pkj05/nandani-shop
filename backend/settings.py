b169b3d32de188b1da2dffc2959de9cee0dbaadb
pkj05 <ghangal.pankaj@gmail.com>
2026-01-19 16:17:32 +0530

    Fixed static root and hosts

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for backend project.
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_ALL_ORIGINS = True
